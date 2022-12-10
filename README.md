c5f8f7d55a3f67cb1dfbad9510be1f811d4d3081
octo <dlqhdgkr456@gmail.com>
2022-12-10 16:52:12 +0900

    setting for web3oj

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # web3oj-sol
+
+web3oj URI: https://app.web3oj.com  
+
+Try running some of the following tasks:  
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.ts
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    // goerli 추가
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
